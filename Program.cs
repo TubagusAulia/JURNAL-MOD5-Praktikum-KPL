c8e2d9bb734625f5639f7d570b81dbdad7bb89bf
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-18 08:07:39 +0700

    SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class SimpleDataBase <T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase() 
+    {
+        this.storedData = new List<T>();
+        this.inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData ()
+    {
+        for (int i = 0; i < this.storedData.Count; i++)
+        {
+            Console.WriteLine($"Data {i + 1} berisi {storedData[i].ToString()}, yang disimpan pada waktu {inputDates[i].ToString()}");
+        }
+    }
+}
+
+class Program
+{
+    private static void Main ()
+    {
+        SimpleDataBase<float> data = new SimpleDataBase<float>();
+        data.AddNewData(10);
+        data.AddNewData(30);
+        data.AddNewData(20);
+        data.PrintAllData();
+    }
+}
